ec5cc5e4214b3e7f9bcf462c21f195f883f7053c
maica-jons <130396885+maica-jons@users.noreply.github.com>
2023-05-04 17:23:06 -0300

    invitado

---
+++ Invitado.py
@@ -0,0 +1,17 @@
+class Invitado():
+
+    lista_invitados = []
+
+    def __init__(self, nombre, apellido, dni, email, cantidad_ingresos =0):  
+        self.nombre = nombre
+        self.apellido = apellido
+        self.dni = dni
+        self.email = email
+        self.cantidad_ingresos = cantidad_ingresos
+     
+     def __str__(self):
+        return (pass)
+    
+
+    def modificar_datos(self):
+        pass
