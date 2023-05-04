d7f9ec620cd47f8ecdaadb4bd50ffd8b29b9ccde
maica-jons <130396885+maica-jons@users.noreply.github.com>
2023-05-04 17:38:01 -0300

    invitado

--- Invitado.py
+++ Invitado.py
@@ -1,8 +1,9 @@
 class Invitado():
 
     lista_invitados = []
+    lista_dni_mail = [] #que tenga adentro listas de 2 elementos: dni y mail
 
-    def __init__(self, nombre, apellido, dni, email, cantidad_ingresos =0):  
+     def __init__(self, nombre, apellido, dni, email, cantidad_ingresos =0):  
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni
@@ -13,5 +14,10 @@ class Invitado():
         return (pass)
     
 
-    def modificar_datos(self):
+     def actualizar_datos(self):
+          #algo con lista_dni_mail []
+          if nunca ingreso:
+               print("El ")
+          
+     
         pass
